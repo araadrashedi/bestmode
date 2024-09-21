a8d404ed6a5f0b2899d2d659c1ba5f7d366154f6
Araad <araad.rashedi@gmail.com>
2024-09-21 14:44:52 +0330

    chore: remove todo comments

--- src/main.tsx
+++ src/main.tsx
@@ -9,20 +9,6 @@ import { createApolloClient } from "@/lib/configs"
 import { ScrollUpButton } from "@/lib/features/scroll_up_button"
 import { ThemeModeToggle } from "@/lib/features/theme_mode_toggle"
 
-// TODO:
-// 01. Add defaults ✅
-// 02. Add login ✅
-// 02.1 Add logout ✅
-// 02.3 unit tests
-// 02.4 integration tests
-// 03. JSDoc ✅
-// 03.1 Biome ✅
-// 04. env ✅
-// 04.1 env validation ✅
-// 04.2 virtual list ✅
-// 04.3 add readme ✅
-// 07. remove extra packages ✅
-
 const client = createApolloClient()
 const router = createBrowserRouter(routes)
 
